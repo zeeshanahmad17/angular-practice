320f8e425821b2c77512f9e4f6a54c932c000d48
Zeeshan Ahmad <za920829@gmail.com>
2023-07-27 20:47:18 +0500

    main v1 - project organized

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms'
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { AssignmentTwoComponent } from './assignment-two/assignment-two.component';
-import { AssignmentThreeComponent } from './assignment-three/assignment-three.component';
-import { AssignmentFourComponent } from './assignment-four/assignment-four.component';
-
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent,
-    AssignmentTwoComponent,
-    AssignmentThreeComponent,
-    AssignmentFourComponent
   ],
   imports: [
     BrowserModule,
