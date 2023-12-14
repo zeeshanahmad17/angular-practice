350f295f4d832199d22e4efaab3537745387a210
Zeeshan Ahmad <za920829@gmail.com>
2023-12-15 01:58:02 +0500

    15 - forms practice - till Array of Form Controls (211)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+// Forms Imports
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+// Routing Imports
+import { Routes, RouterModule } from '@angular/router';
+// Components
 import { AppComponent } from './app.component';
 import { ServicepComponent } from './servicesPractice/servicep.component';
 import { NewAccountComponent } from './servicesPractice/new-account/new-account.component';
 import { AccountComponent } from './servicesPractice/account/account.component';
-import { AccountService } from './servicesPractice/services/account.service';
-import { LoggingService } from './servicesPractice/services/logging.service';
 import { ServicesAssignmentComponent } from './servicesPractice/servicesAssignment/servicesAssignment.component';
-import { ActiveUsersComponent } from './servicesPractice/servicesAssignment/active-users/active-users.component';
 import { InactiveUsersComponent } from './servicesPractice/servicesAssignment/inactive-users/inactive-users.component';
-import { UserService } from './servicesPractice/servicesAssignment/services/users.service';
-import { CounterService } from './servicesPractice/servicesAssignment/services/counter.service';
+import { ActiveUsersComponent } from './servicesPractice/servicesAssignment/active-users/active-users.component';
 import { RoutingPracticeComponent } from './routingPractice/routingPractice.component';
 import { HomeComponent } from './routingPractice/home/home.component';
 import { UsersComponent } from './routingPractice/users/users.component';
@@ -19,17 +19,23 @@ import { ServersComponent } from './routingPractice/servers/servers.component';
 import { UserComponent } from './routingPractice/users/user/user.component';
 import { EditServerComponent } from './routingPractice/servers/edit-server/edit-server.component';
 import { ServerComponent } from './routingPractice/servers/server/server.component';
-import { ServersService } from './routingPractice/servers/servers.service';
-import { Routes, RouterModule } from '@angular/router';
-import { AppRoutingModule } from './app-routing.module';
 import { NotFoundComponent } from './routingPractice/not-found/not-found.component';
+import { ErrorPageComponent } from './routingPractice/error-page/error-page.component';
+import { FormsPracticeComponent } from './forms-practice/forms-practice.component';
+import { TdformsAssignmentComponent } from './forms-practice/tdforms-assignment/tdforms-assignment.component';
+import { ReactiveFormsComponent } from './forms-practice/reactive-forms/reactive-forms.component';
+// Modules
+import { AppRoutingModule } from './app-routing.module';
+// Services
+import { AccountService } from './servicesPractice/services/account.service';
+import { LoggingService } from './servicesPractice/services/logging.service';
+import { UserService } from './servicesPractice/servicesAssignment/services/users.service';
+import { CounterService } from './servicesPractice/servicesAssignment/services/counter.service';
+import { ServersService } from './routingPractice/servers/servers.service';
 import { AuthGuard } from './auth-guard.service';
 import { AuthService } from './auth.service';
 import { CanDeactivateGuard } from './routingPractice/servers/edit-server/can-deactivate-guard.service';
-import { ErrorPageComponent } from './routingPractice/error-page/error-page.component';
 import { ServerResolver } from './routingPractice/servers/server/server-resolver.service';
-import { FormsPracticeComponent } from './forms-practice/forms-practice.component';
-import { TdformsAssignmentComponent } from './forms-practice/tdforms-assignment/tdforms-assignment.component';
 
 @NgModule({
   declarations: [
@@ -55,8 +61,9 @@ import { TdformsAssignmentComponent } from './forms-practice/tdforms-assignment/
     ErrorPageComponent,
     FormsPracticeComponent,
     TdformsAssignmentComponent,
+    ReactiveFormsComponent,
   ],
-  imports: [BrowserModule, FormsModule, AppRoutingModule],
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule, AppRoutingModule],
   providers: [
     AccountService,
     LoggingService,
---
+++ src/app/forms-practice/reactive-forms/reactive-forms.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-reactive-forms',
+  templateUrl: './reactive-forms.component.html',
+  styleUrls: ['./reactive-forms.component.css'],
+})
+export class ReactiveFormsComponent implements OnInit {
+  genders = ['male', 'female'];
+  signupForm: FormGroup;
+
+  ngOnInit(): void {
+    this.signupForm = new FormGroup({
+      userData: new FormGroup({
+        username: new FormControl(null, Validators.required),
+        email: new FormControl(null, [Validators.required, Validators.email]),
+      }),
+      gender: new FormControl('male'),
+      hobbies: new FormArray([]),
+    });
+  }
+
+  onAddHobby(): void {
+    const control = new FormControl(null, Validators.required);
+    (<FormArray>this.signupForm.get('hobbies')).push(
+      new FormControl(null, Validators.required)
+    );
+  }
+
+  onSubmit() {
+    console.log(this.signupForm.value);
+  }
+}
